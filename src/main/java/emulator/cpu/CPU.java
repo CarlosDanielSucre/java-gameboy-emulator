c62eb61a67fc57b49729562087c78f49f11e52ff
Carlos <carlossucredev@gmail.com>
2026-08-24 22:43:07 -0300

    feat(cpu): implement Flags class and start CPU skeleton

--- src/main/java/emulator/cpu/CPU.java
+++ src/main/java/emulator/cpu/CPU.java
@@ -1,4 +1,15 @@
 package cpu;
 
 public class CPU {
+    private Registers registers;
+    private Flags flags;
+
+    public CPU() {
+        this.registers = new Registers();
+        this.flags = new Flags();
+    }
+
+    public boolean calculateHalfCarry(int a, int b) {
+        return ((a & 0xF) + (b & 0xF) > 0xF);
+    }
 }
--- src/main/java/emulator/cpu/Flags.java
+++ src/main/java/emulator/cpu/Flags.java
@@ -1,4 +1,54 @@
 package cpu;
 
 public class Flags {
+    private boolean zero, subtract, halfCarry, carry;
+
+    public boolean isZero() {
+        return zero;
+    }
+
+    public void setZero(boolean zero) {
+        this.zero = zero;
+    }
+
+    public boolean isSubtract() {
+        return subtract;
+    }
+
+    public void setSubtract(boolean subtact) {
+        this.subtract = subtact;
+    }
+
+    public boolean isHalfCarry() {
+        return halfCarry;
+    }
+
+    public void setHalfCarry(boolean halfCarry) {
+        this.halfCarry = halfCarry;
+    }
+
+    public boolean isCarry() {
+        return carry;
+    }
+
+    public void setCarry(boolean carry) {
+        this.carry = carry;
+    }
+
+    public int toByte() {
+        int result = 0;
+        if (zero) result |= (1 << 7);
+        if (subtract) result |= (1 << 6);
+        if (halfCarry) result |= (1 << 5);
+        if (carry) result |= (1 << 4);
+        return result;
+    }
+
+    public void fromByte(int value) {
+        value = value & 0xFF;
+        zero = ((value & (1 << 7)) != 0);
+        subtract = ((value & (1 << 6)) != 0);
+        halfCarry = ((value & (1 << 5)) != 0);
+        carry = ((value & (1 << 4)) != 0);
+    }
 }
